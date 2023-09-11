a08960f89ccae6ec084240db21536e01e633a4e8
komron96 <komron.saydaliev@gmail.com>
2023-09-11 18:14:48 +0500

    Homework24

--- Program.cs
+++ Program.cs
@@ -1,1 +1,53 @@
-﻿Console.WriteLine("ss");
+﻿public sealed class Vehicle
+{
+    private readonly List<DetailPart> _detailsList;
+    public string Name { get; set; }
+    public string Company { get; set; }
+    public string Color { get; set; }
+    public string MaxSpeed { get; set; }
+
+    //Методы удаления, добавления и получения деталей
+    public void AddDetail(string name)
+    {
+        _detailsList.Add(new DetailPart() { DetailName = name });
+    }
+    public DetailPart GetDetail(string name)
+    {
+        return _detailsList.Find(item => item.DetailName == name);
+    }
+    public void Delete(string name)
+    {
+        var detail = _detailsList.FirstOrDefault(item => item.DetailName == name);
+        if (detail is not null)
+        {
+            _detailsList.Remove(detail);
+        }
+    }
+
+    public bool ChangeDescription(string name, string newDesctription)
+    {
+        DetailPart DetailWithNewDesc = _detailsList.Find(item => item.DetailName == name);
+        if (DetailWithNewDesc is null) return false;
+        DetailWithNewDesc.Description = newDesctription;
+        return true;
+    }
+
+    //Индексатор
+    public DetailPart this[string name]   //У индексатора нет названия и используем this
+    {
+        get => _detailsList.Find(item => item.DetailName == name);
+        set { DetailPart detailpart = _detailsList.Find(item => item.DetailName == name); detailpart.DetailName = value.DetailName; }
+    }
+
+    public IEnumerator<DetailPart> GetEnumerator()
+    {
+        foreach(DetailPart detailPart in _detailsList)
+            yield return detailPart;
+    }
+}
+public sealed class DetailPart
+{
+    public string DetailName { get; set; }
+    public string Weight { get; set; }
+    public string Description { get; set; }
+}
