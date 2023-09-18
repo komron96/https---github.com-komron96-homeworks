9c7880589efebe693084be6ba0fb5cb712d04ec2
komron96 <komron.saydaliev@gmail.com>
2023-09-18 23:17:52 +0500

    hmw29

--- Program.cs
+++ Program.cs
@@ -1,63 +1,30 @@
-﻿Vehicle Komas = new Vehicle();
-Komas.AddDetail("Колёса");
-Komas.AddDetail("Руль");
-Komas.AddDetail("Окна");
-Komas.AddDetail("Двери");   
+﻿//Task1
+using System.Runtime.CompilerServices;
+using System.Text.RegularExpressions;
 
-Komas["Двери"] = new DetailPart {DetailName = "Копот"};
+int[] numbers = new int[] { 21, 33, 455, 23, 522, 63, 52, -22, 34, -432, 102, -445, 65, -5123, 105, 25, 740, 105, 42, 99, 355, 56 };
+var task1 = (from number in numbers where number >= 10 && number <= 99 orderby number select number).ToList();
+task1.ForEach(x => Console.WriteLine(x));
 
+//Task2
+string[] strs = new[] { "TOMS", "BOBDD", "SAMSS", "TIDDM", "THOMASS", "BILL", "ALEXADNER", "KOMRON", "TWISTEDMETAL", "GAMEOFCLOWNS" };
+var task2 = strs.OrderBy(p => p.Length).ThenByDescending(p => p).ToList();
+task2.ForEach(x => Console.WriteLine(x));
 
-public sealed class Vehicle
-{
-    private readonly List<DetailPart> _detailsList;
-    public string Name { get; set; }
-    public string Company { get; set; }
-    public string Color { get; set; }
-    public string MaxSpeed { get; set; }
+//Task3
+//Unfortunately cant solve
 
-    //Методы удаления, добавления и получения деталей
-    public void AddDetail(string name)
-    {
-        _detailsList.Add(new DetailPart() { DetailName = name });
-    }
-    public DetailPart GetDetail(string name)
-    {
-        return _detailsList.Find(item => item.DetailName == name);
-    }
-    public void Delete(string name)
-    {
-        var detail = _detailsList.FirstOrDefault(item => item.DetailName == name);
-        if (detail is not null)
-        {
-            _detailsList.Remove(detail);
-        }
-    }
-    public bool ChangeDescription(string name, string newDesctription)
-    {
-        DetailPart DetailWithNewDesc = _detailsList.Find(item => item.DetailName == name);
-        if (DetailWithNewDesc is null) return false;
-        DetailWithNewDesc.Description = newDesctription;
-        return true;
-    }
+//Task4
+var task4 = numbers.Where(p => p > 0).Select(p => p % 10).First();
 
-    //Индексатор - используются чтоб проиндексировать какой либо массив и изменить их свойства при необходимости
-    public DetailPart this[string name]   //У индексатора нет названия и используем this 
-    //Данные индекстор в классе позволяет изменить Имя уже существующей детали
-    {
-        get => _detailsList.Find(item => item.DetailName == name);
-        set { DetailPart detailpart = _detailsList.Find(item => item.DetailName == name); detailpart.DetailName = value.DetailName; }
-    }
-    public IEnumerator<DetailPart> GetEnumerator()
-    {
-        foreach(DetailPart detailPart in _detailsList)
-            yield return detailPart;
-            
-    }
-    
-}
-public sealed class DetailPart
-{
-    public string DetailName { get; set; }
-    public string Weight { get; set; }
-    public string Description { get; set; }
-}
+//Task5
+var task5 = strs.Select(s => s.Length % 2 != 0 ? s[0] : s[s.Length - 1]);
+
+//Task6
+int K1 = 433;
+int K2 = 129;
+
+List<int> A = new List<int> { 552, 6321, 8432,1322,1355, 43,333 };
+List<int> B = new List<int> { 754, 923, 11523, 52313, 16223, 4333,344};
+
+var task6 = A.Where(p=> p>K1).Where(p=>p<K2).OrderBy(p=>p);
