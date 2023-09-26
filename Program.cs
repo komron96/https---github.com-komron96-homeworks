b9f0cbe7fdc8bf6ac46e2839e9d1726e698ee470
komron96 <komron.saydaliev@gmail.com>
2023-09-26 17:30:48 +0500

    Hm30

--- Program.cs
+++ Program.cs
@@ -1,30 +1,216 @@
-﻿//Task1
-using System.Runtime.CompilerServices;
-using System.Text.RegularExpressions;
+﻿//task1
 
-int[] numbers = new int[] { 21, 33, 455, 23, 522, 63, 52, -22, 34, -432, 102, -445, 65, -5123, 105, 25, 740, 105, 42, 99, 355, 56 };
-var task1 = (from number in numbers where number >= 10 && number <= 99 orderby number select number).ToList();
-task1.ForEach(x => Console.WriteLine(x));
+using System.Globalization;
 
-//Task2
-string[] strs = new[] { "TOMS", "BOBDD", "SAMSS", "TIDDM", "THOMASS", "BILL", "ALEXADNER", "KOMRON", "TWISTEDMETAL", "GAMEOFCLOWNS" };
-var task2 = strs.OrderBy(p => p.Length).ThenByDescending(p => p).ToList();
-task2.ForEach(x => Console.WriteLine(x));
+Thread from1To100 = new Thread(DoWork1);
+Thread from101To200 = new Thread(DoWork2);
+from1To100.Start();
+from101To200.Start();
+static void DoWork1()
+{
+    for (int i = 1; i <= 100; i++)
+    {
+        Console.WriteLine("Поток 1" + i);
+        Thread.Sleep(1000);
+    }
+}
+static void DoWork2()
+{
+    for (int i = 101; i <= 200; i++)
+    {
+        Console.WriteLine("Поток 2: " + i);
+        Thread.Sleep(1000);
+    }
+}
 
-//Task3
-//Unfortunately cant solve
 
-//Task4
-var task4 = numbers.Where(p => p > 0).Select(p => p % 10).First();
+//task2
+Thread evennumbers = new Thread(DoWork12);
+Thread oddnumbers = new Thread(DoWork22);
+evennumbers.Start();
+oddnumbers.Start();
+static void DoWork12()
+{
+    for (int i = 1; i <= 99; i++)
+    {
+        if (i % 2 == 0)
+        {
+            Console.WriteLine("Поток 1" + i);
+            Thread.Sleep(1000);
+        }
+    }
+}
+static void DoWork22()
+{
+    for (int i = 1; i <= 99; i++)
+        if (i % 2 != 0)
+        {
+            Console.WriteLine("Поток 1" + i);
+            Thread.Sleep(1000);
+        }
+}
 
-//Task5
-var task5 = strs.Select(s => s.Length % 2 != 0 ? s[0] : s[s.Length - 1]);
 
-//Task6
-int K1 = 433;
-int K2 = 129;
+//task3
+Thread from1To33 = new Thread(DoWork13);
+Thread from34To66 = new Thread(DoWork23);
+Thread from67To99 = new Thread(DoWork33);
+from1To33.Start();
+from34To66.Start();
+from67To99.Start();
+static void DoWork13()
+{
+    for (int i = 1; i <= 33; i++)
+    {
+        Console.WriteLine("Поток 1" + i);
+        Thread.Sleep(1000);
+    }
+}
+static void DoWork23()
+{
+    for (int i = 34; i <= 66; i++)
+    {
+        Console.WriteLine("Поток 2: " + i);
+        Thread.Sleep(1000);
+    }
+}
+static void DoWork33()
+{
+    for (int i = 67; i <= 99; i++)
+    {
+        Console.WriteLine("Поток 2: " + i);
+        Thread.Sleep(1000);
+    }
+}
 
-List<int> A = new List<int> { 552, 6321, 8432,1322,1355, 43,333 };
-List<int> B = new List<int> { 754, 923, 11523, 52313, 16223, 4333,344};
+//task4
+Thread AM = new Thread(DoWork13);
+Thread MZ = new Thread(DoWork23);
+AM.Start();
+MZ.Start();
 
-var task6 = A.Where(p=> p>K1).Where(p=>p<K2).OrderBy(p=>p);
+static void AM1()
+{
+    for (char letter = 'A'; letter <= 'M'; letter++)
+    {
+        Console.WriteLine("Поток 1: " + letter);
+    }
+}
+
+static void MZ1()
+{
+    for (char letter = 'N'; letter <= 'Z'; letter++)
+    {
+        Console.WriteLine("Поток 2: " + letter);
+    }
+}
+
+
+//task5
+Thread divisionOf3 = new(Dowork51);
+Thread divisionOf5 = new(Dowork52);
+
+static void Dowork51()
+{
+    for(int i=1; i < 100; i++)
+    {
+        if(i % 3 ==0)
+            Console.WriteLine("Потом:" +i);
+    }
+}
+static void Dowork52()
+{
+
+    for(int i=1; i < 100; i++)
+    {
+        if(i % 5 ==0)
+            Console.WriteLine("Потом:" +i);
+    }
+}
+
+//task6
+//Таск повторяется 
+
+//task7
+Thread sumOf = new(Dowork71);
+Thread mupltiplicationOf = new(Dowork72);
+
+static void Dowork71()
+{
+    int number = 0;
+    for(int i=1; i < 100; i++)
+    {
+        number +=i;
+    }
+    Console.WriteLine("Сумма: " + number);
+}
+static void Dowork72()
+{
+    int number = 0;
+    for(int i=1; i < 100; i++)
+    {
+        number *=i;
+    }
+    Console.WriteLine("Сумма: " + number);
+}
+
+//task8
+Thread divisionOf3And5 = new(Dowork81);
+Thread NotDivisionOf3and5 = new(Dowork82);
+
+static void Dowork81()
+{
+    for(int i=1; i < 100; i++)
+    {
+        if (i % 3 == 0 || i % 5 == 0)
+            Console.WriteLine("Потом:" +i);
+    }
+}
+static void Dowork82()
+{
+
+    for(int i=1; i < 100; i++)
+    {
+        if (i % 3 != 0 && i % 5 != 0)
+            Console.WriteLine("Потом:" +i);
+    }
+}
+
+//task9
+Thread from50To1 = new Thread(DoWork91);
+Thread from100To51 = new Thread(DoWork92);
+
+static void DoWork91()
+{
+    for (int i = 50; i <= 1; i--)
+    {
+        Console.WriteLine("Поток 1" + i);
+    }
+}
+static void DoWork92()
+{
+    for (int i = 100; i <= 51; i--)
+    {
+        Console.WriteLine("Поток 2: " + i);
+    }
+}
+
+//task10
+Thread from01To100 = new Thread(DoWork101);
+Thread ResultOfmultiplictionOfThread1 = new Thread(DoWork102);
+
+static void DoWork101()
+{
+    for (int i = 1; i <= 100; i++)
+    {
+        Console.WriteLine("Поток 1" + i);
+    }
+}
+static void DoWork102()
+{
+    for (int i = 1; i <= 100; i++)
+    {
+        int number = i*i;
+        Console.WriteLine("Поток 2: " + number);
+    }
+}
