80c4af21e18927b1539eb43ad95c6f92ee5673dc
komron96 <komron.saydaliev@gmail.com>
2023-09-28 15:06:09 +0500

    part2

--- Program.cs
+++ Program.cs
@@ -1,12 +1,12 @@
-﻿//task1
-Thread thread1 = new(Action1) //созданный тред у которого есть свойство приоритета, который в свою очередь принимает значение Normal.
+﻿//task1,6
+Thread thread1 = new(Action1) //созданный тред у которого есть свойство приоритета, который в свою очередь принимает значение Highest.
 {
-    Priority = ThreadPriority.Normal
+    Priority = ThreadPriority.Highest
 };
 
 Thread thread2 = new(Action1)
 {
-    Priority = ThreadPriority.BelowNormal //Иной тред который принимает значение ниже нормального и будет выполняться после Normal
+    Priority = ThreadPriority.BelowNormal //Иной тред который принимает значение ниже нормального и будет выполняться после Normal или всё что выше normal включая highest
 };
 
 static void Action1() //метод который реализует какое то событие
@@ -18,26 +18,61 @@ static void Action1() //метод который реализует какое 
     }
 }
 
-//task2 - Отмена управляемых потоков
+//task2, 7 - Отмена управляемых потоков
 //У приложения есть MAIN трэд и есть ветки
 
-CancellationTokenSource cancell = new();
+CancellationTokenSource cancell = new CancellationTokenSource();
 //Для отмены трэда создаётся ТОКЕН(struct) который выбрасывает исключение
 
-Thread thread3 = new(Action3);
-thread3.Start();
+Thread thread3 = new Thread(Action3);
+thread3.Start(cancell.Token); //Запускаем тред и передаём в него созданный токен
 
-if (Console.ReadKey().Key == ConsoleKey.Escape)
+while (true)
 {
-    cancell.Cancel();
+    if (Console.ReadKey().Key == ConsoleKey.Escape)
+    {
+        cancell.Cancel();
+        break;
+    }
 }
 void Action3(object boxedToken)
 {
-    for (int i = 0; i < 1000; i++)
+    try
     {
-        Thread.Sleep(1000);
-        CancellationToken token = (CancellationToken) boxedToken;
-        token.ThrowIfCancellationRequested();
-        Console.WriteLine("Itteration: " + i);
+        CancellationToken token = (CancellationToken)boxedToken;
+        for (int i = 0; i < 1000; i++)
+        {
+            Thread.Sleep(1000);
+            token.ThrowIfCancellationRequested();
+            Console.WriteLine("Itteration: " + i);
+        }
+    }
+    //task5 - Так как треды начинаются выполняться с мейн треда то исключения которые присходят в доп тредах не могут пойманы мейнтредом
+    //Поэтому исключения должны обрабатываться только в методах которые являются входным параметром самого треда.
+    catch (System.Exception exception)
+    {
+        Console.WriteLine(exception.Message);
+    }
+}  //Созданная программа запускает цикл через тред который начаинется считать до 1000 и если нажать escape то будет исключение
+
+//task 3
+
+Thread thread4 = new(Action5);
+thread4.Start();
+Thread.Sleep(2000);  //Запускается поток который выполяет метод Action5 (цикл выполнения работы) и через 2 секунды он прерывается абортится
+thread4.Abort();
+void Action5()
+{
+    try
+    {
+        while (true)
+        {
+            Console.WriteLine("Р - значит работа");
+            Thread.Sleep(1000);
+        }
+    }
+    catch (ThreadAbortException)
+    {
+        Console.WriteLine("Aborted");
     }
 }
