c2ea3a4cb769345f2a03867a38f6f03ea7a1bd35
komron96 <komron.saydaliev@gmail.com>
2023-10-05 17:52:43 +0500

    Fix

--- Process.cs
+++ Process.cs
@@ -6,7 +6,7 @@ public class DataProcessor
 {
     public List<string> RateQueryExecution(NpgsqlConnection conn)
     {
-        string sqlQuery = "SELECT TO_CHAR(DATE(datetime), 'DD.MM.YYYY') AS formatted_date, TO_CHAR(official_rate, 'FM999999999.00') AS official_rate, currency_from, currency_to FROM exchange_rates WHERE datetime BETWEEN '2023-09-29' AND '2023-10-01' AND type = 'standard' AND currency_from IN ('RUB','USD', 'EUR') AND currency_to = 'TJS';  ";
+        string sqlQuery = "SELECT DISTINCT TO_CHAR(DATE(datetime), 'DD.MM.YYYY') AS formatted_date, TO_CHAR(official_rate, 'FM999999999.00') AS official_rate, currency_from, currency_to FROM exchange_rates WHERE datetime BETWEEN '2023-09-29' AND '2023-10-01' AND type = 'standard' AND currency_from IN ('RUB','USD', 'EUR') AND currency_to = 'TJS';  ";
         List<string> rateList = new List<string>();
 
         using (var cmd = new NpgsqlCommand(sqlQuery, conn))
@@ -33,6 +33,7 @@ public class DataProcessor
     {
         string sqlQuery = "SELECT TO_CHAR(DATE(datetime), 'DD.MM.YYYY') AS formatted_date, amount, account_currency from fimi_transaction WHERE datetime BETWEEN '2023-09-01 00:00:00' AND '2023-09-01 23:59:09' AND tran_code NOT IN ('624', '801') AND (tran_type <> '420' OR tran_number NOT IN ( SELECT DISTINCT tran_number FROM fimi_transaction WHERE datetime BETWEEN '2023-09-01 00:00:00' AND '2023-09-01 23:59:09' AND tran_type = '420')) and tran_number in ('324400381536', '324400222004', '324447494025', '324403101003', '324400400170', '324450465642');";
         List<string> fimiList = new List<string>();
+        
         using (var cmd = new NpgsqlCommand(sqlQuery, conn))
         {
             using (var reader = cmd.ExecuteReader())
@@ -40,13 +41,11 @@ public class DataProcessor
                 while (reader.Read())
                 {
                 string date = reader.GetString(0);
-                object amountObject = reader.GetValue(1); // Используйте GetDecimal для считывания числовых значений
+                decimal amount = reader.GetDecimal(1); 
                 decimal currency = reader.GetDecimal(2);
-                
-                string amountString = (amountObject == DBNull.Value) ? "N/A" : amountObject.ToString();
 
                 fimiList.Add(date);
-                fimiList.Add(amountObject.ToString()); // Преобразуйте числовое значение в строку
+                fimiList.Add(amount.ToString());
                 fimiList.Add(currency.ToString());
                 }
             }
