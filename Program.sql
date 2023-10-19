989281a99f4eb43aaec1742254b1b044995e1a46
komron96 <komron.saydaliev@gmail.com>
2023-10-19 16:06:53 +0500

    ss

---
+++ Program.sql
@@ -0,0 +1,28 @@
+﻿--1
+SELECT cus.id, cus.name, cus.lastname
+FROM Customers cus
+INNER JOIN Orders ord ON cus.customer_id = ord.customer_id
+INNER JOIN OrderDetails OrDet ON ord.order_id = OrDetON.order_id;
+
+--2
+select sum(amount)
+from orders
+where created_at between '2023-09-01' and '2023-09-30'
+
+--3
+SELECT cus.id
+FROM Customers cus
+left join Orders ord ON cus.customer_id = ord.customer_id
+where order_id is null
+
+--4
+select sum(amount)
+from orders
+left join OrderDetails OrDet ON ord.order_id = OrDet.order_id;
+where OrDetON.city = "Moscow"
+
+--5
+SELECT cus.customer_id, cus.name, COUNT(Orders.order_id) AS total_orders
+FROM Customers cus
+LEFT JOIN Orders or ON cus.customer_id = or.id
+GROUP BY cus.id, cus.name;
