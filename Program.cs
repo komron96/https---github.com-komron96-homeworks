a5f677f3512b61a9495cd6136cb4489a5de51142
komron96 <komron.saydaliev@gmail.com>
2024-01-30 11:42:08 +0500

    2 methods were added

--- Program.cs
+++ Program.cs
@@ -1,7 +1,41 @@
-public static class PaymentSystem
+public class PaymentSystem
 {
-    public static void Payment(string productName, decimal amount, string phoneNumber, decimal creditRange)
+
+    public decimal Fee(decimal amount, int initialPeriod, decimal extraPayment, int minPeriod, int maxPeiod)
     {
-        
+        if (initialPeriod < minPeriod || initialPeriod > maxPeiod)
+        {
+            throw new ArgumentException($"Inccorect credit period format");
+        }
+        else
+        {
+            int extraMonths = initialPeriod - minPeriod;
+            decimal fee = extraMonths * extraPayment * amount;
+            return fee;
+        }
     }
+    public decimal Payment(string productName, decimal amount, string phoneNumber, decimal creditRange, int period)
+    {
+        decimal totalAmount = amount;
+
+        if (productName == "SmartPhone")
+        {
+            _ = Fee(amount, period, 0.03m, 3, 9);
+        }
+        else if (productName == "TV")
+        {
+            _ = Fee(amount, period, 0.04m, 3, 12);
+        }
+        else if (productName == "Computer")
+        {
+            _ = Fee(amount, period, 0.05m, 3, 18);
+        }
+        else
+        {
+            throw new ArgumentException($"Wrong produc");
+        }
+        return totalAmount;
+    }
+
+
 }
